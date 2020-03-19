48afa4d9d94dd9dec96b16de5443d53291c898b1
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-19 20:48:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -164,7 +164,7 @@ def player_numbers(team_names)
 end
 
 def player_stats(player_n)
-game_hash.each do |home_away, keys|
+game_hash.each do |home_away, keys| #home_away = :home or :away; keys = :team_name, :colors, or :players
     keys[:players].each do |single_player|
       if single_player[:player_name] == player_n
         return single_player.delete_if { |stat, value| [:player_name].include?(stat)}
