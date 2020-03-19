3e299cafad9d48c1a1f399b664eafc80f4933d70
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-19 20:20:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -147,7 +147,7 @@ def num_points_scored(player_name)
     end
 
 def team_names
-  game_hash.map do |key, value| #place = :home or :away;
+  game_hash.map do |key, value| #key = :home or :away; value = team hash
     value[:team_name]
     binding.pry
   end
