487626c4cc8a0758034d2f6aed606b1950fc09a2
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-19 20:42:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -174,13 +174,13 @@ game_hash.each do |home_away, keys|
   end
 end
 
-player_stats("Alan Anderson")
-  => { :number => 0,
-        :shoe => 16,
-        :points => 22,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 3,
-        :blocks => 1,
-        :slam_dunks => 1
-      }
+# player_stats("Alan Anderson")
+#   => { :number => 0,
+#         :shoe => 16,
+#         :points => 22,
+#         :rebounds => 12,
+#         :assists => 12,
+#         :steals => 3,
+#         :blocks => 1,
+#         :slam_dunks => 1
+#       }
