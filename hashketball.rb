e21c38a4837be674f8d7b6f0021225d30f1ed822
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-19 16:05:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,11 +4,13 @@ def game_hash
   {
   :home => {
     :team_name => "",
-    :colors => []
+    :colors => [],
+    :players => []
   },
   :away => {
     :team_name => "",
-    :colors => []
+    :colors => [],
+    :players => []
   }
   }
 end
