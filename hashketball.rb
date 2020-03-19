19c1c4678fbad07c332a7fa57c5a8b985875e86e
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-19 16:32:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,14 +17,14 @@ def game_hash
          :slam_dunks => 1
          },
          {:player_name => "Reggie Evans",
-        :number => 0,
-        :shoe => 0,
-        :points => 0,
-        :rebounds => 0,
-        :assists => 0,
-        :steals => 0,
-        :blocks => 0,
-        :slam_dunks =>0
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
         },
         {:player_name => "Brook Lopez",
          :number => 0,
