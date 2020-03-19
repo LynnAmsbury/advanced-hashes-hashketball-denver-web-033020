251491eb4a44e58764e1035149d8610905ca872f
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-19 20:35:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -155,7 +155,7 @@ end
 def player_numbers(team_names)
   game_hash.each do |home_away, team_hash| #home_away = :home or :away; team_hash = team hash
     if team_hash[:team_name] == team_names
-      return team_hash[:players].map do |player|
+      return team_hash[:players].map do |player| #team_hash[:players] = :players ARRAY
         player[:number]
         binding.pry
       end
