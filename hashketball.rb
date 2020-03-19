d2635c719126ff5ec3a3c62900512d5ab577bfcc
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-19 17:27:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def game_hash
- game_hash = {
+  {
   :home => {
     :team_name => "Brooklyn Nets",
     :colors => ["black", "white"],
@@ -115,22 +115,10 @@ def game_hash
       ]
     }
   }
-  game_hash
 end
 
-def num_points_scored(player_name)
-  binding.pry
-  game_hash.each do |location, team_data|
-    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+def num_points_scored(name)
+  game_hash.each do |place, team|
     binding.pry
-    team_data.each do |attribute, data|
-      #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-      binding.pry
-
-      #what is 'data' at each loop throughout .each block? when will the following line of code work and when will it break?
-      # data.each do |data_item|
-      #     binding.pry
-      # end
-    end
   end
 end
