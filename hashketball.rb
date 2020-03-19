e2aaa8723eff0f03a563514f6931ef32c5024cb5
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-19 20:25:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -121,11 +121,11 @@ def num_points_scored(player_name)
   game_hash.each do |place, team| #place = :home or :away; team = team hash
       team[:players].each do |single_player| #single_player is the player stat hash
         if player_name == single_player[:player_name]
-          return single_player[:points]
-        end
+        return single_player[:points]
       end
     end
   end
+end
 
 def shoe_size(player_name)
   game_hash.each do |place, team| #place = :home or :away; team = team hash
