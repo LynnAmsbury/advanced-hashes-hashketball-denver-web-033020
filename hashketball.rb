061a177e09622a9c275fae45978eca848741da21
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-19 19:20:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -141,6 +141,7 @@ def num_points_scored(player_name)
     def team_colors(team_name)
       game_hash.each do |place, team| #place = :home or :away; team = team hash
         if team[:team_name] == team_name
+          binding.pry
           return team[:colors]
         end
       end
